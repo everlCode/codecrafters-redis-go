36a40a116c5b7deb09c1f6fca3cebc6f6e2109d6
everl <kolukul1996@gmail.com>
2026-03-30 14:09:58 +0300

    rpush append item

--- app/handlers/rpush.go
+++ app/handlers/rpush.go
@@ -9,17 +9,22 @@ type RpushCommand struct {
 }
 
 func (c RpushCommand) Execute(args []resp.Value, db *db.DB) resp.Value {
-
 	key := args[0]
 	if key.Type != resp.BULK {
 		return resp.Value{Type: resp.ERROR, String: "Key should be string!"}
 	}
 
 	arg := args[1]
-	value := resp.Value{
-		Type: resp.ARRAY,
-		Array: []resp.Value{arg},
+	value, ok := db.Sets[key.Bulk]
+	if !ok {
+		value = resp.Value{
+			Type: resp.ARRAY,
+			Array: []resp.Value{arg},
+		}
+	} else {
+		value.Array = append(value.Array, arg)
 	}
+	
 	len := len(value.Array)
 
 	db.Sets[key.Bulk] = value
