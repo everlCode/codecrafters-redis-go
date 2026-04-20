dac623a66169142bfbfd72f11ac74dc8c05061fe
everl <kolukul1996@gmail.com>
2026-04-20 21:30:11 +0300

    fix lpush

--- app/handlers/lpush.go
+++ app/handlers/lpush.go
@@ -13,18 +13,20 @@ func (c LpushCommand) Execute(args []resp.Value, db *database.DB) resp.Value {
 	if key.Type != resp.BULK {
 		return resp.Value{Type: resp.ERROR, String: "Key should be string!"}
 	}
-
+	
 	entry, ok := db.Get(key.Bulk)
 	if !ok {
-		return resp.Array([]string{})
+		entry = database.Array([]string{})
 	}
+
 	argss := resp.ParseSlice(args)
-	for i := 0; i < len(argss); i++ {
+	for i := range argss {
 		data := entry.AsArray()
 		
 		var a = []string{argss[i]}
 		entry.Set(append(a, data...))
 	}
+	
 
 	arr := entry.AsArray()
 	lenght := len(arr)
