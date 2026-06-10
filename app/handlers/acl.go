c87002d751aef687e040bd4c7c67af99e4902064
n.kulakov <nikolay.kulakov@dreamjob.ru>
2026-06-10 17:33:45 +0300

    codecrafters submit [skip ci]

--- app/handlers/acl.go
+++ app/handlers/acl.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"strings"
+
 	"github.com/codecrafters-io/redis-starter-go/app/clients"
 	"github.com/codecrafters-io/redis-starter-go/app/resp"
 	"github.com/codecrafters-io/redis-starter-go/app/server"
@@ -10,5 +12,19 @@ type AclCommand struct {
 }
 
 func (c AclCommand) Execute(args []string, server *server.Server, client *clients.Client) CommandResponse {
+	_type := args[0]
+
+	switch strings.ToLower(_type) {
+	case "whoami":
+		return Response(resp.Bulk("default"))
+	case "getuser":
+		return Response(resp.Array(
+			[]any{
+				"flags", []any{},
+			},
+		))
+	}
+
+
     return Response(resp.Bulk("default"))
 }
